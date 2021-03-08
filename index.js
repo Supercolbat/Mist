2c5d1c9bbb5ca7e6f8bbc389174e2774bfcbdf08
Supercolbat <supercolbat@gmail.com>
2021-03-08 08:56:21 -0600

    Security fixes + LF endings

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const { app, BrowserWindow } = require("electron")
+const path = require("path")
 
 function createWindow() {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
         minHeight: 75,
+
         webPreferences: {
-            nodeIntegration: true
+            nodeIntegration: false,
+            contextIsolation: true,
+            enableRemoteModule: false,
+            preload: path.join(app.getAppPath(), "windowController.js")
         },
 
         transparent: true,
